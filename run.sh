64012644c4c28fabc2f6c7124fb0be9cf8781ed3
Yaroslav Martynenko <stikblacklabel@gmail.com>
2021-05-24 23:48:06 +0300

    fix run script

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo Applying migrations
 python manage.py migrate
-python manage.py collectstatic --noinput
+echo Starting server
 gunicorn beer_site.wsgi:application -b 0.0.0.0:${1:-8000} -w 1
