ffef7b40585103738d99b63d557358a72839e13c
Yaroslav Martynenko <stikblacklabel@gmail.com>
2021-05-26 09:55:21 +0300

    fix ci

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-ssh -o "StrictHostKeyChecking=no" ${SSH_USER}@${SSH_ADDRESS} -i ~/.ssh/id_rsa << 'ENDSSH' 
+ssh -o "StrictHostKeyChecking=no" ${SSH_USER}@${SSH_ADDRESS} -tt -i ~/.ssh/id_rsa << 'ENDSSH' 
     echo Start Deployment
     ls
     docker pull ${DOCKER_USERNAME}/beer-site-533-backend:${TRAVIS_BRANCH:-latest}
